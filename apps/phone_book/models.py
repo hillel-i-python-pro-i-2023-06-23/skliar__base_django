0e2f4218e24b00767b017a2cc3159e99d8649821
nick <nikoladarkangel@gmail.com>
2023-09-08 18:55:21 +0300

    finish 0.0.2

--- apps/phone_book/models.py
+++ apps/phone_book/models.py
@@ -9,8 +9,13 @@ class ContactType(models.Model):
 
 
 class ContactData(models.Model):
-    contact_type = models.ForeignKey(ContactType, on_delete=models.CASCADE)
+    contact_type = models.ForeignKey("ContactType", on_delete=models.CASCADE)
     value = models.CharField(max_length=255)  # Здесь можно добавить валидацию по типу контакта
+    contact = models.ForeignKey(
+        "Contact",
+        related_name="contact_data",
+        on_delete=models.CASCADE,
+    )  # Связь с Contact моделью
 
     def __str__(self):
         return self.value
@@ -26,8 +31,8 @@ class Group(models.Model):
 class Contact(models.Model):
     name = models.CharField(max_length=100, verbose_name="Имя")
     birthdate = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
-    groups = models.ManyToManyField(Group, verbose_name="Группы")
-    contact_data = models.ManyToManyField(ContactData, verbose_name="Контактные данные")
+    groups = models.ManyToManyField("Group", verbose_name="Группы")
+    # contact_data = models.ManyToManyField('ContactData', verbose_name="Контактные данные")
 
     def __str__(self):
         return self.name
--- apps/phone_book/services/generate_data.py
+++ apps/phone_book/services/generate_data.py
@@ -1,28 +1,46 @@
+from django.db import transaction
 from apps.phone_book.models import Contact, ContactType, Group, ContactData
 from faker import Faker
+import random
+
+fake = Faker()
 
 
 def generate_phones():
-    fake = Faker()
-    contact_types = ["Email", "Phone", "Address"]  # Замените на ваши типы контактов
+    # Определите модели ContactType, Group, Contact и ContactData здесь
+
+    # Ваши типы контактов
+    contact_types = ["Email", "Phone", "Address"]
 
     # Генерация данных для модели ContactType
     for type_name in contact_types:
         ContactType.objects.get_or_create(name=type_name)
 
     # Генерация данных для модели Group
-    for _ in range(10):  # Создадим 10 групп
+    for _ in range(10):
         Group.objects.get_or_create(name=fake.word())
 
-    # Генерация данных для модели Contact и ContactData
-    for _ in range(10):  # Создадим 100 контактов
-        contact = Contact.objects.create(
-            name=fake.name(),
-            birthdate=fake.date_of_birth(minimum_age=18, maximum_age=80, tzinfo=None),
-        )
-        contact.groups.add(*Group.objects.order_by("?")[:3])  # Добавляем 3 случайные группы
+    # Генерация данных для моделей Contact и ContactData
+    with transaction.atomic():
+        for _ in range(10):
+            contact = Contact.objects.create(
+                name=fake.name(),
+                birthdate=fake.date_of_birth(minimum_age=18, maximum_age=80),
+            )
+            contact.groups.add(*random.sample(list(Group.objects.all()), 3))
 
-        for _ in range(3):  # Создадим 3 случайных контактных данных для каждого контакта
-            contact_data_type = ContactType.objects.get(name=fake.random_element(contact_types))
-            contact_data = ContactData.objects.create(contact_type=contact_data_type, value=fake.email())
-            contact.contact_data.add(contact_data)  # Связываем контакт с контактными данными
+            for _ in range(3):
+                contact_data_type = ContactType.objects.get(name=random.choice(contact_types))
+                if contact_data_type.name == "Phone":
+                    contact_data_value = fake.phone_number()
+                elif contact_data_type.name == "Address":
+                    contact_data_value = fake.address()
+                else:
+                    contact_data_value = fake.email()
+
+                # Создание ContactData и связь с Contact
+                contact_data = ContactData.objects.create(  # noqa
+                    contact=contact,
+                    contact_type=contact_data_type,
+                    value=contact_data_value,
+                )
