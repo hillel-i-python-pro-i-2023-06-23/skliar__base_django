e351ccd0a299c30f1b7dbe86e05c1a03a0128f6d
nick <nikoladarkangel@gmail.com>
2023-09-12 19:07:04 +0300

    finish env

--- Makefile
+++ Makefile
@@ -13,7 +13,8 @@ d-homework-i-purge:
 .PHONY: init-configs
 # Configuration files initialization
 init-configs:
-	@cp docker-compose.override.dev.yml docker-compose.override.yml
+	@cp .env.homework .env &&\
+	cp docker-compose.override.dev.yml docker-compose.override.yml
 
 
 .PHONY: d-run
--- core/settings.py
+++ core/settings.py
@@ -9,32 +9,44 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import string
 from pathlib import Path
 from typing import List
 import environ
-
-env = environ.FileAwareEnv()
+from django.utils.crypto import get_random_string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR.joinpath("apps")
-environ.Env.read_env(BASE_DIR.joinpath(".env"))
+
+env = environ.FileAwareEnv()
+env.read_env(BASE_DIR.joinpath(".env"))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)17n(--lun@6=829!*%(qe1m+8s&)0r=(qa0v0i3exz7t5oh$u"
+SECRET_KEY = (
+    env.str(
+        "DJANGO__SECRET_KEY",
+        get_random_string(64, "".join([string.ascii_letters, string.digits, string.punctuation])),
+    ),
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO__DEBUG", False)
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "0.0.0.0",
-]
+ALLOWED_HOSTS = env.list("DJANGO__ALLOWED_HOSTS", default=[])
+if DEBUG:
+    ALLOWED_HOSTS.extend(
+        [
+            "localhost",
+            "127.0.0.1",
+            "0.0.0.0",
+        ],
+    )
 
 
 # Application definition
